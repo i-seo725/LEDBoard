55db2bb04a09d5c7dc880d68a4791e82823987a5
i-seo725 <hjing719@gmail.com>
2023-07-24 17:06:06 +0900

    Initial Commit

---
+++ LEDBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LEDBoard
+//
+//  Created by 이은서 on 2023/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
