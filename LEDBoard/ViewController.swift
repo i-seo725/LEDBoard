b391fe7d7caaf441e2125e2e4890b842d5aee840
i-seo725 <hjing719@gmail.com>
2023-07-24 17:30:35 +0900

    프로젝트 레이아웃 세팅

--- LEDBoard/ViewController.swift
+++ LEDBoard/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var searchAreaView: UIView!
+    @IBOutlet var wordTextField: UITextField!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var randomTextStyleButton: UIButton!
+    @IBOutlet var resultLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        designTextField()
+        designButton(button: sendButton, title: "Send", color: UIColor.black)
+        designButton(button: randomTextStyleButton, title: "Aa", color: UIColor.red)
+        searchAreaView.layer.cornerRadius = 10
+        designResultLabel()
     }
+    
+    func designTextField() {
+        wordTextField.layer.borderWidth = 0
+        wordTextField.layer.borderColor = UIColor.white.cgColor
 
+    }
+    func designButton(button: UIButton, title: String, color: UIColor) {
+        button.setTitle(title, for: .normal)
+        button.backgroundColor = .white
+        button.tintColor = color
+        button.layer.cornerRadius = 20
+        button.layer.borderColor = UIColor.black.cgColor
+        button.layer.borderWidth = 2
+    }
+    func designResultLabel() {
+        resultLabel.textAlignment = .center
+        resultLabel.numberOfLines = 0
+        resultLabel.font = UIFont.systemFont(ofSize: 80)
+        resultLabel.text = ""
+//        resultLabel.textColor = .white
+    }
 
 }
 
